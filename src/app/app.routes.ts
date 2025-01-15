7a4cdc8d6c75af94e4f7aa6a80c6a74265abe004
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2025-01-15 14:07:14 +0100

    pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UploadComponent } from './pages/upload/upload.component';
+import { ResultComponent } from './pages/result/result.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: UploadComponent
+  },
+  {
+    path: 'result',
+    component: ResultComponent
+  }
+];
